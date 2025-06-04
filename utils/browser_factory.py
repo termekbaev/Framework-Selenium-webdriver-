5d9995bfa014a1f4cb6075e50b6050b27814ed01
Timur <t-ermekbaev@mail.ru>
2025-06-05 00:46:46 +0300

    feat: Прописал класс BrowserFactory с выбором между chrome и firefox. И перенес туда инициализацию

---
+++ utils/browser_factory.py
@@ -0,0 +1,63 @@
+from selenium import webdriver
+from utils.config_reader import ConfigReader
+
+class BrowserFactory:
+    CHROME_HEADLESS = "--headless=new"
+    CHROME_INCOGNITO = "--incognito"
+    CHROME_DISABLE_EXTENSIONS = "--disable-extensions"
+    CHROME_LANG = "--lang={}"
+    CHROME_WINDOW_SIZE = "--window-size={}"
+
+    FIREFOX_HEADLESS = "--headless"
+    FIREFOX_PRIVATE = "--private"
+    FIREFOX_DISABLE_EXTENSIONS = "--disable-extensions"
+    FIREFOX_LANG_PREFERENCE = "intl.accept_languages"
+
+    def __init__(self, config: ConfigReader):
+        self._config = config
+
+    def create_driver(self):
+        browser_name = self._config.app_config.browser.lower()
+
+        if browser_name == "chrome":
+            return self._create_chrome_driver()
+        elif browser_name == "firefox":
+            return self._create_firefox_driver()
+        raise ValueError(f"Unsupported browser: {browser_name}. Use 'chrome' or 'firefox'")
+
+    def _create_chrome_driver(self) -> webdriver.Chrome:
+        options = webdriver.ChromeOptions()
+        cfg = self._config.app_config.chrome_options
+
+        if cfg.headless:
+            options.add_argument(self.CHROME_HEADLESS)
+        if cfg.incognito:
+            options.add_argument(self.CHROME_INCOGNITO)
+        if cfg.disable_extensions:
+            options.add_argument(self.CHROME_DISABLE_EXTENSIONS)
+
+        options.add_argument(self.CHROME_LANG.format(cfg.lang))
+        options.add_argument(self.CHROME_WINDOW_SIZE.format(cfg.window_size))
+        options.page_load_strategy = cfg.page_load_strategy
+
+        driver = webdriver.Chrome(options=options)
+        driver.maximize_window()
+        return driver
+
+    def _create_firefox_driver(self) -> webdriver.Firefox:
+        options = webdriver.FirefoxOptions()
+        cfg = self._config.app_config.firefox_options
+
+        if cfg.headless:
+            options.add_argument(self.FIREFOX_HEADLESS)
+        if cfg.private:
+            options.add_argument(self.FIREFOX_PRIVATE)
+        if cfg.disable_extensions:
+            options.add_argument(self.FIREFOX_DISABLE_EXTENSIONS)
+
+        options.set_preference(self.FIREFOX_LANG_PREFERENCE, cfg.lang)
+        options.page_load_strategy = cfg.page_load_strategy
+
+        driver = webdriver.Firefox(options=options)
+        driver.set_window_size(*map(int, cfg.window_size.split(',')))
+        return driver
--- utils/driver_manager.py
+++ utils/driver_manager.py
@@ -1,5 +1,5 @@
 from utils.config_reader import ConfigReader
-from selenium import webdriver
+from utils.browser_factory import BrowserFactory
 
 class SingletonMeta(type):
     _instances = {}
@@ -9,15 +9,7 @@ class SingletonMeta(type):
             cls._instances[cls] = super().__call__(*args, **kwargs)
         return cls._instances[cls]
 
-
 class DriverManager(metaclass=SingletonMeta):
-    CHROME_HEADLESS = "--headless=new"
-    CHROME_INCOGNITO = "--incognito"
-    CHROME_DISABLE_EXTENSIONS = "--disable-extensions"
-    CHROME_LANG_TEMPLATE = "--lang={}"
-    CHROME_WINDOW_SIZE_TEMPLATE = "--window-size={}"
-    CHROME_PAGE_LOAD_STRATEGY = "{}"
-
     def __init__(self, config: ConfigReader):
         self._config = config
         self._driver = None
@@ -29,22 +21,7 @@ class DriverManager(metaclass=SingletonMeta):
         return self._driver
 
     def _init_driver(self):
-        options = webdriver.ChromeOptions()
-
-        if self._config.app_config.chrome_options.headless:
-            options.add_argument(self.CHROME_HEADLESS)
-        if self._config.app_config.chrome_options.incognito:
-            options.add_argument(self.CHROME_INCOGNITO)
-        if self._config.app_config.chrome_options.disable_extensions:
-            options.add_argument(self.CHROME_DISABLE_EXTENSIONS)
-        options.add_argument(self.CHROME_LANG_TEMPLATE.format(self._config.app_config.chrome_options.lang))
-        options.add_argument(self.CHROME_WINDOW_SIZE_TEMPLATE.format(self._config.app_config.chrome_options.window_size))
-        options.page_load_strategy = self.CHROME_PAGE_LOAD_STRATEGY.format(self._config.app_config.chrome_options.page_load_strategy)
-        for arg in self._config.app_config.chrome_options.additional_args:
-            options.add_argument(arg)
-
-        self._driver = webdriver.Chrome(options=options)
-        self._driver.maximize_window()
+        self._driver = BrowserFactory(self._config).create_driver()
 
     def quit(self):
         if self._driver is not None:
