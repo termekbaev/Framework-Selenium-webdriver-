de192012f8bd2d70106f37255eb4c576c7344963
Timur <t-ermekbaev@mail.ru>
2025-06-09 23:06:58 +0300

    feat: Прописаны логи в тестовых файлах(пока что только начало, успешное завершение, либо ошибка теста)

--- tests/test_demoqa_alerts.py
+++ tests/test_demoqa_alerts.py
@@ -12,38 +12,45 @@ def generate_random_text(length: int = 20) -> str:
     return ''.join(random.choice(string.ascii_letters) for _ in range(length))
 
 def test_alerts(driver_manager: DriverManager, config: ConfigReader) -> None:
-    driver = driver_manager.driver
-    driver.get(config.app_config.main_url)
-    
-    main_page = MainPage(driver)
-    assert main_page.is_opened(), "Main page not opened"
-    
-    alerts_frame_and_windows_page = main_page.open_alerts_frame_and_windows_page()
-    assert alerts_frame_and_windows_page.is_opened(), "Alerts, Frame and Windows page not opened"
+    logger.info(f"Starting test Demoqa Alerts")
+    try:
+        driver = driver_manager.driver
+        driver.get(config.app_config.main_url)
+        
+        main_page = MainPage(driver)
+        assert main_page.is_opened(), "Main page not opened"
+        
+        alerts_frame_and_windows_page = main_page.open_alerts_frame_and_windows_page()
+        assert alerts_frame_and_windows_page.is_opened(), "Alerts, Frame and Windows page not opened"
 
-    alerts_page = alerts_frame_and_windows_page.open_alerts_section()
-    assert alerts_page.is_opened(), "Alerts section not opened"
+        alerts_page = alerts_frame_and_windows_page.open_alerts_section()
+        assert alerts_page.is_opened(), "Alerts section not opened"
 
-    alerts_util = AlertUtil(driver)
+        alerts_util = AlertUtil(driver)
 
-    alerts_page.click_alert_button()
-    alert_text = alerts_util.get_alert_text()
-    alerts_util.accept_alert()
-    assert alert_text == "You clicked a button", "Incorrect alert text"
-    assert not alerts_util.is_alert_present(), "Alert not closed"
+        alerts_page.click_alert_button()
+        alert_text = alerts_util.get_alert_text()
+        alerts_util.accept_alert()
+        assert alert_text == "You clicked a button", "Incorrect alert text"
+        assert not alerts_util.is_alert_present(), "Alert not closed"
 
-    alerts_page.click_confirm_button()
-    confirm_text = alerts_util.get_alert_text()
-    alerts_util.accept_alert()
-    confirm_result = alerts_page.get_confirm_result_text()
-    assert confirm_text == "Do you confirm action?", "Incorrect confirm text"
-    assert "Ok" in confirm_result, "Confirm result not shown, or not 'OK'"
+        alerts_page.click_confirm_button()
+        confirm_text = alerts_util.get_alert_text()
+        alerts_util.accept_alert()
+        confirm_result = alerts_page.get_confirm_result_text()
+        assert confirm_text == "Do you confirm action?", "Incorrect confirm text"
+        assert "Ok" in confirm_result, "Confirm result not shown, or not 'OK'"
 
-    alerts_page.click_prompt_button()
-    prompt_text = alerts_util.get_alert_text()
-    random_text = generate_random_text()
-    alerts_util.send_text_to_alert(random_text)
-    alerts_util.accept_alert()
-    prompt_result = alerts_page.get_prompt_result_text()
-    assert prompt_text == "Please enter your name", "Incorrect prompt text"
-    assert prompt_result == f"You entered {random_text}", "Prompt result mismatch"
+        alerts_page.click_prompt_button()
+        prompt_text = alerts_util.get_alert_text()
+        random_text = generate_random_text()
+        alerts_util.send_text_to_alert(random_text)
+        alerts_util.accept_alert()
+        prompt_result = alerts_page.get_prompt_result_text()
+        assert prompt_text == "Please enter your name", "Incorrect prompt text"
+        assert prompt_result == f"You entered {random_text}", "Prompt result mismatch"
+        
+        logger.info("Test completed successfully")
+    except Exception as e:
+        logger.error(f"Test failed: {str(e)}")
+        raise
--- tests/test_demoqa_frames.py
+++ tests/test_demoqa_frames.py
@@ -6,23 +6,30 @@ import logging
 logger = logging.getLogger(__name__)
 
 def test_frames(driver_manager: DriverManager, config: ConfigReader) -> None:
-    driver = driver_manager.driver
-    driver.get(config.app_config.main_url)
-    
-    main_page = MainPage(driver)
-    assert main_page.is_opened(), "Main page not opened"
+    logger.info(f"Starting test Demoqa Frames")
+    try:    
+        driver = driver_manager.driver
+        driver.get(config.app_config.main_url)
+        
+        main_page = MainPage(driver)
+        assert main_page.is_opened(), "Main page not opened"
 
-    alerts_frame_and_windows_page = main_page.open_alerts_frame_and_windows_page()
-    assert alerts_frame_and_windows_page.is_opened(), "Alerts, Frame and Windows page not opened"
-    nested_frames_page = alerts_frame_and_windows_page.open_nested_frames_section()   
-    assert nested_frames_page.is_opened(), "Nested frames page not opened"
+        alerts_frame_and_windows_page = main_page.open_alerts_frame_and_windows_page()
+        assert alerts_frame_and_windows_page.is_opened(), "Alerts, Frame and Windows page not opened"
+        nested_frames_page = alerts_frame_and_windows_page.open_nested_frames_section()   
+        assert nested_frames_page.is_opened(), "Nested frames page not opened"
 
-    outer_frame_text, inner_frame_text = nested_frames_page.get_text_in_frames()
-    assert "Parent frame" == outer_frame_text and "Child Iframe" == inner_frame_text, "Some of frame texts not found"
+        outer_frame_text, inner_frame_text = nested_frames_page.get_text_in_frames()
+        assert "Parent frame" == outer_frame_text and "Child Iframe" == inner_frame_text, "Some of frame texts not found"
 
-    frames_page = alerts_frame_and_windows_page.open_frames_section()
-    assert frames_page.is_opened(), "Frames page not opened"
+        frames_page = alerts_frame_and_windows_page.open_frames_section()
+        assert frames_page.is_opened(), "Frames page not opened"
 
-    top_frame_text = frames_page.switch_and_get_top_frame_text()
-    bottom_frame_text = frames_page.switch_and_get_bottom_frame_text()
-    assert top_frame_text == bottom_frame_text, "Frame1 text != Frame2 text"
+        top_frame_text = frames_page.switch_and_get_top_frame_text()
+        bottom_frame_text = frames_page.switch_and_get_bottom_frame_text()
+        assert top_frame_text == bottom_frame_text, "Frame1 text != Frame2 text"
+
+        logger.info("Test completed successfully")
+    except Exception as e:
+        logger.error(f"Test failed: {str(e)}")
+        raise
