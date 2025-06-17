d5f9a063e7bf670ff51f5e38e4dc177e58c1e399
Timur <t-ermekbaev@mail.ru>
2025-06-17 21:42:17 +0300

    fix: Избавился от дублирования кода методов

--- pages/frames_page.py
+++ pages/frames_page.py
@@ -2,24 +2,23 @@ from pages.base_page import BasePage
 from pages.sample_page import SamplePage
 from utils.frame_util import FrameUtil
 from selenium.webdriver.common.by import By
+from typing import Tuple
 
 class FramesPage(BasePage):
     UNIQUE_ELEMENT = (By.XPATH, "//*[contains(@class, 'show')]//*[@id='item-2' and contains(@class, 'active')]")
     FRAME_TOP = (By.ID, "frame1")
     FRAME_BOTTOM = (By.ID, "frame2")
 
-    def switch_and_get_top_frame_text(self) -> str:
+    def __get_frame_text(self, frame_locator: Tuple[By, str]) -> str:
         frame_util = FrameUtil()
-        frame_util.switch_to_frame(self.FRAME_TOP)
-        top_frame_page = SamplePage()
-        top_frame_text = top_frame_page.get_sample_page_text()
+        frame_util.switch_to_frame(frame_locator)
+        frame_page = SamplePage()
+        frame_text = frame_page.get_sample_page_text()
         frame_util.switch_to_default_content()
-        return top_frame_text
+        return frame_text
+    
+    def switch_and_get_top_frame_text(self) -> str:
+        return self.__get_frame_text(self.FRAME_TOP)
     
     def switch_and_get_bottom_frame_text(self) -> str:
-        frame_util = FrameUtil()
-        frame_util.switch_to_frame(self.FRAME_BOTTOM)
-        bottom_frame_page = SamplePage()
-        bottom_frame_text = bottom_frame_page.get_sample_page_text()
-        frame_util.switch_to_default_content()
-        return bottom_frame_text
+        return self.__get_frame_text(self.FRAME_BOTTOM)
